e25137618c5c497101fd68f370285b6825c946b4
tristandaly <tristandaly@gmail.com>
2019-12-23 09:58:03 -0600

    Added Method Overload example

---
+++ SmallConverter/src/com/tristandaly/Main.java
@@ -0,0 +1,8 @@
+package com.tristandaly;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(SmallConverter.calcFeetAndInchesToCentimeters(6, 3));
+		SmallConverter.calcFeetAndInchesToCentimeters(100);
+	}
+}
